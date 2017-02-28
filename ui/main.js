c0517de7e071da04de6502cb8df3de48bcc44d8c
Saumil Jain <saumil-jain@users.noreply.github.com>
2017-02-28 10:25:53 +0100

    Moves the image to the right on click.

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "new value";
+
+// move the image
+var img = document.getElementById('madi');
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+}
