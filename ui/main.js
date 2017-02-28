a9ddd49b92b794419846ae4621718572d03d712f
Saumil Jain <saumil-jain@users.noreply.github.com>
2017-02-28 10:32:24 +0100

    Moves the image to the right continuously on click.

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,13 @@ var element = document.getElementById("main-text");
 element.innerHTML = "new value";
 
 // move the image
+
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 50);
 }
