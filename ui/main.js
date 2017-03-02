2171c26f1381013cb736e927dd065028e59db579
Saumil Jain <saumil-jain@users.noreply.github.com>
2017-03-02 12:35:34 +0100

    Adds a counter button on home page. Counter resets on page refresh.

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,8 @@
-console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.innerHTML = "new value";
+var counter = 0;
+var button = document.getElementById("counter");
 
-// move the image
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-}
+button.onclick = function() {
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+};
