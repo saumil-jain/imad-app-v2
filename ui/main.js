a815309a7a7dd2c0c9669eb86f72e7389a6cba42
Saumil Jain <saumil-jain@users.noreply.github.com>
2017-03-03 07:16:51 +0100

    Fixes bug in main.js - missing var keyword.

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick = function() {
     request.onreadystatechange = function() {
         if (request.readystate === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                counter = request.responseText;
+                var counter = request.responseText;
                 var span = document.getElementById("count");
                 span.innerHTML = counter.toString();
             }
